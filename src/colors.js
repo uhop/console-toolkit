8e0c58284b36abafa1f66068a7528cf9b30a9dc1
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2023-07-10 11:14:42 -0500

    Used named literals.

--- src/colors.js
+++ src/colors.js
@@ -1,31 +1,38 @@
 export const findEscSequence = /\x1B\[([\d;]+)m/g;
 
-export const commands = {
-  RESET_ALL: 0,
-  BOLD: 1,
-  DIM: 2,
-  ITALIC: 3,
-  UNDERLINE: 4,
-  BLINKING: 5,
-  INVERSE: 7,
-  HIDDEN: 8,
-  STRIKETHROUGH: 9,
-  RESET_BOLD: 22,
-  RESET_DIM: 22,
-  RESET_ITALIC: 23,
-  RESET_UNDERLINE: 24,
-  RESET_BLINKING: 25,
-  RESET_INVERSE: 27,
-  RESET_HIDDEN: 28,
-  RESET_STRIKETHROUGH: 29,
-  DOUBLE_UNDERLINE: 21,
-  RESET_DOUBLE_UNDERLINE: 24
+export const Commands = {
+  RESET_ALL: '0',
+  BOLD: '1',
+  DIM: '2',
+  ITALIC: '3',
+  UNDERLINE: '4',
+  BLINKING: '5',
+  INVERSE: '7',
+  HIDDEN: '8',
+  STRIKETHROUGH: '9',
+  RESET_BOLD: '22',
+  RESET_DIM: '22',
+  RESET_ITALIC: '23',
+  RESET_UNDERLINE: '24',
+  RESET_BLINKING: '25',
+  RESET_INVERSE: '27',
+  RESET_HIDDEN: '28',
+  RESET_STRIKETHROUGH: '29',
+  DOUBLE_UNDERLINE: '21',
+  RESET_DOUBLE_UNDERLINE: '24',
+  COLOR_EXTENDED: '38',
+  BG_COLOR_EXTENDED: '48',
+  COLOR_DEFAULT: '39',
+  BG_COLOR_DEFAULT: '49'
 };
 
-const resetCommands = [0, 22, 22, 23, 24, 25, , 27, 28, 29];
-resetCommands[21] = 24;
+export const FORMAT_COLOR256 = '5';
+export const FORMAT_TRUE_COLOR = '2';
 
-export const reset = command => resetCommands[typeof command == 'string' ? commands[command.toUpperCase()] : command] || 0;
+const resetCommands = ['0', '22', '22', '23', '24', '25', , '27', '28', '29'];
+resetCommands['21'] = '24';
+
+export const reset = command => resetCommands[typeof command == 'string' ? Commands[command.toUpperCase()] : command] || 0;
 
 export const setCommands = commands => `\x1B[${Array.isArray(commands) ? commands.join(';') : commands}m`;
 
@@ -50,28 +57,28 @@ export const setBrightBgColor = color => setCommands(getBrightBgColor(color));
 const get6 = colorComponent => Math.round(colorComponent / 17);
 const get24 = intensity => Math.max(0, Math.min(23, Math.round((intensity - 8) / 10)));
 
-export const getStdColor256 = color => ['38', '5', colorNumber(color)];
-export const getBrightStdColor256 = color => ['38', '5', 8 + colorNumber(color)];
-export const getColor256 = (r, g, b) => ['38', '5', 16 + 36 * get6(r) + 6 * get6(g) + get6(b)];
-export const getGrayColor256 = i => ['38', '5', 232 + get24(i)];
+export const getStdColor256 = color => [Commands.COLOR_EXTENDED, FORMAT_COLOR256, colorNumber(color)];
+export const getBrightStdColor256 = color => [Commands.COLOR_EXTENDED, FORMAT_COLOR256, 8 + colorNumber(color)];
+export const getColor256 = (r, g, b) => [Commands.COLOR_EXTENDED, FORMAT_COLOR256, 16 + 36 * get6(r) + 6 * get6(g) + get6(b)];
+export const getGrayColor256 = i => [Commands.COLOR_EXTENDED, FORMAT_COLOR256, 232 + get24(i)];
 
 export const setStdColor256 = color => setCommands(getStdColor256(color));
 export const setBrightStdColor256 = color => setCommands(getBrightStdColor256(color));
 export const setColor256 = (r, g, b) => setCommands(getColor256(r, g, b));
 export const setGrayColor256 = i => setCommands(getGrayColor256(i));
 
-export const getStdBgColor256 = color => ['48', '5', colorNumber(color)];
-export const getBrightStdBgColor256 = color => ['48', '5', 8 + colorNumber(color)];
-export const getBgColor256 = (r, g, b) => ['48', '5', 16 + 36 * get6(r) + 6 * get6(g) + get6(b)];
-export const getGrayBgColor256 = i => ['48', '5', 232 + get24(i)];
+export const getStdBgColor256 = color => [Commands.BG_COLOR_EXTENDED, FORMAT_COLOR256, colorNumber(color)];
+export const getBrightStdBgColor256 = color => [Commands.BG_COLOR_EXTENDED, FORMAT_COLOR256, 8 + colorNumber(color)];
+export const getBgColor256 = (r, g, b) => [Commands.BG_COLOR_EXTENDED, FORMAT_COLOR256, 16 + 36 * get6(r) + 6 * get6(g) + get6(b)];
+export const getGrayBgColor256 = i => [Commands.BG_COLOR_EXTENDED, FORMAT_COLOR256, 232 + get24(i)];
 
 export const setStdBgColor256 = color => setCommands(getStdColor256(color));
 export const setBrightStdBgColor256 = color => setCommands(getBrightStdBgColor256(color));
 export const setBgColor256 = (r, g, b) => setCommands(getBgColor256(r, g, b));
 export const setGrayBgColor256 = i => setCommands(getGrayBgColor256(i));
 
-export const getTrueColor = (r, g, b) => ['38', '2', r, g, b];
-export const getTrueBgColor = (r, g, b) => ['48', '2', r, g, b];
+export const getTrueColor = (r, g, b) => [Commands.COLOR_EXTENDED, FORMAT_TRUE_COLOR, r, g, b];
+export const getTrueBgColor = (r, g, b) => [Commands.BG_COLOR_EXTENDED, FORMAT_TRUE_COLOR, r, g, b];
 
 export const setTrueColor = (r, g, b) => setCommands(getTrueColor(r, g, b));
 export const setTrueBgColor = (r, g, b) => setCommands(getTrueBgColor(r, g, b));
