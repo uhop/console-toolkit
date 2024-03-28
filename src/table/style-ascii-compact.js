41a5281892a0dba71d896498baf5a8075bf29476
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-03-28 18:06:40 -0500

    More ASCII styles.

--- src/table/style-ascii-compact.js
+++ src/table/style-ascii-compact.js
@@ -7,3 +7,15 @@ const table11 = {t: ' ', m: ' ', b: ' ', v: ' ', h: '-', w: [0, 1, 0]},
 
 populateTableSymbols(style, table11, 1, 1);
 populateTableSymbols(style, table21, 2, 1);
+
+const markdownGithub11 = {t: '|||', m: '|||', b: '|||', v: '|||', h: '-'},
+  markdownGithub21 = {t: '|||', m: '|||', b: '|||', v: '|||', h: '='};
+
+populateTableSymbols(style, markdownGithub11, 'mg1', 'mg1');
+populateTableSymbols(style, markdownGithub21, 'mg2', 'mg1');
+
+const markdownReddit11 = {t: '|', m: '|', b: '|', v: '|', h: '-', w: [0, 1, 0]},
+  markdownReddit21 = {t: '|', m: '|', b: '|', v: '|', h: '=', w: [0, 1, 0]};
+
+populateTableSymbols(style, markdownReddit11, 'mr1', 'mr1');
+populateTableSymbols(style, markdownReddit21, 'mr2', 'mr1');
--- src/table/style-ascii-simple.js
+++ src/table/style-ascii-simple.js
@@ -7,3 +7,17 @@ const table11 = {t: '+++', m: '+++', b: '+++', v: '|||', h: '-'},
 
 populateTableSymbols(style, table11, 1, 1);
 populateTableSymbols(style, table21, 2, 1);
+
+const rounded11 = {t: '...', m: ':+:', b: "'''", v: '|||', h: '-'},
+  rounded21 = {t: '...', m: ':+:', b: "'''", v: '|||', h: '='};
+
+populateTableSymbols(style, rounded11, 'r1', 'r1');
+populateTableSymbols(style, rounded21, 'r2', 'r1');
+
+const dots = {t: '...', m: '.:.', b: ':::', v: ':::', h: '.'};
+
+populateTableSymbols(style, dots, 'd', 'd');
+
+const girder = {t: '//[]\\\\', m: '|][][|', b: '\\\\[]//', v: '||||||', h: '=', w: [2, 2, 2]};
+
+populateTableSymbols(style, girder, 'g', 'g');
