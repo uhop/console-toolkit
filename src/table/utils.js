582c143f7ef75f380caadfc30a8f15a1d2f21120
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-03-28 21:23:24 -0500

    New utilities.

--- src/table/utils.js
+++ src/table/utils.js
@@ -1,5 +1,3 @@
-export const getLength = (matcher, s) = String(s).replace(matcher, '').length;
-
 // The line symbols are indexed in this order: tmb
 // t is top
 // m is middle
---
+++ src/utils.js
@@ -0,0 +1,9 @@
+const matchCsiNoGroups = /\x1B\[[\x30-\x3F]*[\x20-\x2F]*[\x40-\x7E]/g;
+
+export const getLength = (s, matcher = matchCsiNoGroups) => {
+  let captured = 0;
+  for (const match of String(s).matchAll(matcher)) {
+    captured += match[0].length;
+  }
+  return s.length - captured;
+};
