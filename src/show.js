e4c351458c4839ba3298a23eb5121dd0658ad096
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-03-30 00:32:08 -0500

    Fixed the end of line command not appended to lines.

--- src/show.js
+++ src/show.js
@@ -4,7 +4,6 @@ export const log = (s, endOfLineCommand = '\x1B[m', isMonochrome) => {
   if (typeof s == 'string') s = s.split(/\r?\n/g);
   if (s && typeof s == 'object' && typeof s.toBox == 'function') s = s.toBox('', ' ');
   s.forEach(row => {
-    if (isMonochrome) row = (row + endOfLineCommand).replace(matchCsiNoGroups, '');
-    console.log(row);
+    console.log(isMonochrome ? (row + endOfLineCommand).replace(matchCsiNoGroups, '') : row + endOfLineCommand);
   });
 };
