ed27d5eb0eb332b668fd03754ec8b4a13b5c3909
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-03-30 00:15:36 -0500

    More tests.

--- tests/manual/test-screen-0.js
+++ tests/manual/test-screen-0.js
@@ -4,6 +4,11 @@ import {getBgColor, Colors} from '../../src/sgr.js';
 import {newState} from '../../src/sgr-state.js';
 
 const screen = new Screen(8, 4);
-screen.fill(2, 1, 4, 2, ' ', newState([getBgColor(Colors.RED)]))
 
+screen.put(0, 0, '12345678').put(0, 1, '12345678').put(0, 2, '12345678').put(0, 3, '12345678');
 log(screen);
+console.log('==');
+
+screen.fill(2, 1, 4, 2, '*', newState([getBgColor(Colors.RED)]));
+log(screen);
+console.log('==');
