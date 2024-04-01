73c25855d71930dbdf904b056c055004f0385ed3
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-04-01 17:43:55 -0500

    Added streams. WiP.

--- src/show.js
+++ src/show.js
@@ -1,9 +1,34 @@
+import process from 'node:process';
 import {matchCsiNoGroups} from './utils.js';
 
 export const log = (s, endOfLineCommand = '\x1B[m', isMonochrome) => {
   if (typeof s == 'string') s = s.split(/\r?\n/g);
   if (s && typeof s == 'object' && typeof s.toBox == 'function') s = s.toBox('', ' ');
-  s.forEach(row => {
-    console.log(isMonochrome ? (row + endOfLineCommand).replace(matchCsiNoGroups, '') : row + endOfLineCommand);
-  });
+  if (isMonochrome) {
+    s.forEach(row => console.log((row + endOfLineCommand).replace(matchCsiNoGroups, '')));
+    return;
+  }
+  s.forEach(row => console.log(row + endOfLineCommand));
 };
+
+export const out = (s, stream = process.stdout, endOfLineCommand = '\x1B[m', isMonochrome) => {
+  if (typeof isMonochrome != 'boolean') isMonochrome = !(stream.isTTY && stream.hasColors(256));
+  if (typeof s == 'string') s = s.split(/\r?\n/g);
+  if (s && typeof s == 'object' && typeof s.toBox == 'function') s = s.toBox('', ' ');
+  if (isMonochrome) {
+    s.forEach(row => stream.write((row + endOfLineCommand).replace(matchCsiNoGroups, '') + '\n'));
+    return;
+  }
+  s.forEach(row => stream.write(row + endOfLineCommand + '\n'));
+};
+
+export class Out {
+  constructor(stream) {
+    this.stream = stream;
+    this.isMonochrome = !(stream.isTTY && stream.hasColors(256));
+  }
+  out(s, endOfLineCommand = '\x1B[m', isMonochrome) {
+    if (typeof isMonochrome != 'boolean') isMonochrome = this.isMonochrome;
+    return out(s, this.stream, endOfLineCommand, isMonochrome);
+  }
+}
