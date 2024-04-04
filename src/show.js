781d3f572dab981086e689c96c145ad8d83ed07a
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-04-04 12:43:23 -0500

    Switched from monochrome to color depth.

--- src/show.js
+++ src/show.js
@@ -1,21 +1,21 @@
 import process from 'node:process';
 import {matchCsiNoGroups} from './ansi/utils.js';
 
-export const log = (s, endOfLineCommand = '\x1B[m', isMonochrome) => {
+export const log = (s, endOfLineCommand = '\x1B[m', colorDepth = 24) => {
   if (typeof s == 'string') s = s.split(/\r?\n/g);
   if (s && typeof s == 'object' && typeof s.toBox == 'function') s = s.toBox('', ' ');
-  if (isMonochrome) {
+  if (colorDepth < 4) {
     s.forEach(row => console.log((row + endOfLineCommand).replace(matchCsiNoGroups, '')));
     return;
   }
   s.forEach(row => console.log(row + endOfLineCommand));
 };
 
-export const out = (s, stream = process.stdout, endOfLineCommand = '\x1B[m', isMonochrome) => {
-  if (typeof isMonochrome != 'boolean') isMonochrome = !(stream.isTTY && stream.hasColors(256));
+export const out = (s, stream = process.stdout, endOfLineCommand = '\x1B[m', colorDepth) => {
+  if (typeof colorDepth != 'number') colorDepth = stream.isTTY ? stream.getColorDepth() : 1;
   if (typeof s == 'string') s = s.split(/\r?\n/g);
   if (s && typeof s == 'object' && typeof s.toBox == 'function') s = s.toBox('', ' ');
-  if (isMonochrome) {
+  if (colorDepth < 4) {
     s.forEach(row => stream.write((row + endOfLineCommand).replace(matchCsiNoGroups, '') + '\n'));
     return;
   }
@@ -25,10 +25,10 @@ export const out = (s, stream = process.stdout, endOfLineCommand = '\x1B[m', isM
 export class Out {
   constructor(stream) {
     this.stream = stream;
-    this.isMonochrome = !(stream.isTTY && stream.hasColors(256));
+    this.colorDepth = stream.isTTY ? stream.getColorDepth() : 1;
   }
-  out(s, endOfLineCommand = '\x1B[m', isMonochrome) {
-    if (typeof isMonochrome != 'boolean') isMonochrome = this.isMonochrome;
-    return out(s, this.stream, endOfLineCommand, isMonochrome);
+  out(s, endOfLineCommand = '\x1B[m', colorDepth) {
+    if (typeof colorDepth != 'number') colorDepth = this.colorDepth;
+    return out(s, this.stream, endOfLineCommand, colorDepth);
   }
 }
