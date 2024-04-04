d72d9c473ba7e6732375ddac30b589c6d5afc631
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-04-03 22:13:24 -0500

    Added a unit test for tables with merged cells.

---
+++ tests/test-draw-table-skip.js
@@ -0,0 +1,41 @@
+import test from 'tape-six';
+
+import {draw} from '../src/table/index.js';
+import {style} from '../src/table/style-unicode.js';
+
+test('Draw a table with skip rectangles', async t => {
+  await t.test('Round table with two merged cells', t => {
+    const box = draw(
+      style,
+      ['r', 1, 'r', 1, 'r', 1, 'r'],
+      ['r', 1, 'r', 1, 'r', 1, 'r'],
+      [
+        {x: 1, y: 5, width: 3, height: 1},
+        {x: 1, y: 1, width: 3, height: 3}
+      ]
+    );
+
+    t.equal(box.length, 7);
+    t.equal(box[0].length, 7);
+
+    t.deepEqual(box, ['╭───┬─╮', '│   │ │', '│   ├─┤', '│   │ │', '├───┼─┤', '│   │ │', '╰───┴─╯']);
+  });
+
+  await t.test('Double lines outside, single lines inside', t => {
+    const box = draw(
+      style,
+      ['r', 1, 'r', 1, 'r', 1, 'r'],
+      ['r', 1, 'r', 1, 'r', 1, 'r'],
+      [
+        {x: 1, y: 1, width: 2, height: 1},
+        {x: 1, y: 2, width: 1, height: 2},
+        {x: 3, y: 3, width: 3, height: 3}
+      ]
+    );
+
+    t.equal(box.length, 7);
+    t.equal(box[0].length, 7);
+
+    t.deepEqual(box, ['╭───┬─╮', '│   │ │', '│ ╭─┴─┤', '│ │   │', '├─┤   │', '│ │   │', '╰─┴───╯']);
+  });
+});
