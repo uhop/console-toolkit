3eb06bdff44ff9c804d3b9a6faa610117d753a8b
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-04-05 17:54:58 -0500

    Minor style fix.

--- tests/manual/test-style-9.js
+++ tests/manual/test-style-9.js
@@ -1,7 +1,5 @@
 import style from '../../src/style.js';
 
 console.log('Hello, ' + style.bold.bright.cyan.text('bold bright cyan') + '! We love you!');
-
 console.log('Hello, ' + style.add('This is BOLD BRIGHT CYAN: \x1B[1;96m').text('bold bright cyan') + '! We love you!');
-
 console.log('Hello, ' + style.add(style.bold.bright.cyan).text('bold bright cyan') + '! We love you!');
