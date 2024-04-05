854f0cbc2649213287e4ca29a405b1b1e7ab4ff5
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-04-05 11:06:58 -0500

    Refactored `draw()`.

--- tests/manual/utils.js
+++ tests/manual/utils.js
@@ -1,12 +1,17 @@
 import {padBoxLeft} from '../../src/box/pad.js';
 import {stackHorizontally} from '../../src/box/stack.js';
 
+export const drawBox = box => box.forEach(line => console.log(line));
+
 export const draw = (...boxes) => {
+  if (!boxes.length) return;
+  if (boxes.length == 1) return drawBox(boxes[0]);
+
   let result;
   for (const box of boxes) {
     result = result ? stackHorizontally(result, padBoxLeft(box, 2)) : box;
   }
-  result.forEach(line => console.log(line));
+  drawBox(result);
 };
 
 export const show = s =>
