151c8fba765455e3a733e398c6df78a6a5e5ece6
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-04-06 10:15:58 -0500

    Added a command showcase.

---
+++ tests/manual/commands.js
@@ -0,0 +1,42 @@
+import {normalizeBox} from '../../src/box/index.js';
+import {padBoxRight} from '../../src/box/pad.js';
+import {stackHorizontally} from '../../src/box/stack.js';
+import style, {s} from '../../src/style.js';
+import {draw, show} from './utils.js';
+
+console.log(s`{{bold.bright.cyan}}Supported commands{{reset.all}}\n`);
+
+{
+  const names = [style.italic.text('none') + ':'],
+    values = ['sample'];
+  for (const name of 'bold,dim,italic,underline,blink,rapidBlink,inverse,hidden,strikethrough,overline'.split(',')) {
+    names.push(name + ':');
+    values.push(style[name].text('sample'));
+  }
+  draw(stackHorizontally(padBoxRight(normalizeBox(names), 1), normalizeBox(values)));
+}
+
+console.log(s`\n{{bold.bright.cyan}}Underline versions{{reset.all}}\n`);
+
+{
+  const names = [],
+    values = [];
+  for (const name of 'underline,doubleUnderline,curlyUnderline'.split(',')) {
+    names.push(name + ':');
+    values.push(style[name].text('sample'));
+  }
+  draw(stackHorizontally(padBoxRight(normalizeBox(names), 1), normalizeBox(values)));
+}
+
+console.log(s`\n{{bold.bright.cyan}}Underline decoration colors{{reset.all}}\n`);
+
+{
+  const names = [style.italic.text('none') + ':', 'bright yellow:', 'bright red:', 'bright green:'],
+    values = [
+      style.underline.text('sample'),
+      style.doubleUnderline.decoration.bright.yellow.text('sample'),
+      style.curlyUnderline.decoration.bright.red.text('sample'),
+      style.underline.decoration.bright.green.text('sample')
+    ];
+  draw(stackHorizontally(padBoxRight(normalizeBox(names), 1), normalizeBox(values)));
+}
