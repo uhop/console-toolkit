4aff6bcd3071a1b636092cbff3bc0230f3fcf035
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-04-06 00:21:58 -0500

    Added a test for the back quote function.

---
+++ tests/test-style-bq.js
@@ -0,0 +1,30 @@
+import test from 'tape-six';
+
+import {s, RESET_STATE} from '../src/style.js';
+
+test('Styling with back quotes', async t => {
+  await t.test('Plain styling', t => {
+    const text = s`Hello, {{bold.red}}bold red{{defaultColor}} bold{{resetBold}}!`;
+    t.equal(text, 'Hello, \x1B[1;31mbold red\x1B[39m bold\x1B[22m!');
+  });
+
+  await t.test('Styling with save/restore', t => {
+    const text = s`Hello, {{save.bold.save.red}}bold red{{restore}} bold{{restore}}!`;
+    t.equal(text, 'Hello, \x1B[1;31mbold red\x1B[39m bold\x1B[22m!');
+  });
+
+  await t.test('Styling with separate commands', t => {
+    const text = s`Hello, {{save}}{{bold}}{{save}}{{red}}bold red{{restore}} bold{{restore}}!`;
+    t.equal(text, 'Hello, \x1B[1;31mbold red\x1B[39m bold\x1B[22m!');
+  });
+
+  await t.test('Styling with functions and arguments', t => {
+    const text = s`Hello, {{save}}${t => t.bold}{{save.red}}bold red{{restore}} ${'bold'}{{restore}}!`;
+    t.equal(text, 'Hello, \x1B[1;31mbold red\x1B[39m bold\x1B[22m!');
+  });
+
+  await t.test('Styling with RESET_STATE as the beginning', t => {
+    const text = s(RESET_STATE)`Hello, {{save.bold.save.red}}bold red{{restore}} bold{{restore}}!`;
+    t.equal(text, 'Hello, \x1B[1;31mbold red\x1B[39m bold\x1B[m!');
+  });
+});
