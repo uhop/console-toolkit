c7bd4167b07b3578e0e011ec0909e0b48762f32f
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-04-08 10:04:12 -0500

    Used back quotes to style table values.

--- tests/manual/test-data-table-0.js
+++ tests/manual/test-data-table-0.js
@@ -1,14 +1,14 @@
 import Data from '../../src/table/Data.js';
 import lineStyle from '../../src/line-styles/unicode-rounded.js';
-import style from '../../src/style.js';
+import style, {s} from '../../src/style.js';
 import {draw} from './utils.js';
 
 const table = new Data([
-  ['Color', 'Sample', 'Number'],
-  ['red', style.red.text('red'), 1],
-  ['blue', style.blue.text('blue'), 2],
-  ['green', style.green.text('1st line\n2nd\nand 3rd'), 3],
-  [style.inverse.text('bright\nyellow'), style.bright.yellow.text('bright yellow'), '12,345']
+  ['Color', 'Sample', 'Number'].map(x => s`{{bold}}${x}`),
+  ['red', s`{{red}}red`, 1],
+  ['blue', s`{{blue}}blue`, 2],
+  ['green', s`{{green}}1st line\n2nd\nand 3rd`, 3],
+  [s`{{inverse}}bright\nyellow`, s`{{bright.yellow}}bright yellow`, '12,345']
 ], {hAlign: ['l', 'c', 'r'], vAlign: ['t', 'c', 'c', 'c', 'b']});
 table.hAxis[2] = '2';
 table.vAxis[1] = '2';
