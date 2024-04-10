a63552f4e44c8fca59b429c67133aa76cf200962
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-04-10 00:24:31 -0500

    More complicated example: an ASCII art.

--- tests/manual/test-turtle-0.js
+++ tests/manual/test-turtle-0.js
@@ -1,12 +1,19 @@
 import Turtle from '../../src/turtle/Turtle.js';
-import style from '../../src/line-styles/unicode.js';
+import styleUnicode from '../../src/line-styles/unicode.js';
+import styleAsciiRounded from '../../src/line-styles/ascii-rounded.js';
 import drawLineArt from '../../src/turtle/draw-line-art.js';
 import {draw} from './utils.js';
 
-const turtle = new Turtle(5, 5);
+const turtle1 = new Turtle(5, 5);
 // draw single line border
-turtle.forward(4).right().forward(4).right().forward(4).right().forward(4);
+turtle1.forward(4).right().forward(4).right().forward(4).right().forward(4);
 // draw middle cross with double lines
-turtle.setTheme(2).set(2, 0).moveDown(4).set(0, 2).moveRight(4);
+turtle1.setTheme(2).set(2, 0).moveDown(4).set(0, 2).moveRight(4);
 
-draw(drawLineArt(turtle, style));
+const turtle2 = new Turtle(5, 5);
+// draw single line border
+turtle2.forward(4).right().forward(4).right().forward(4).right().forward(4);
+// draw horizontal double lines
+turtle2.set(2, 0).moveDown(4).setTheme(2).set(0, 0).moveRight(4).set(0, 2).moveRight(4).set(0, 4).moveRight(4);
+
+draw(drawLineArt(turtle1, styleUnicode), drawLineArt(turtle2, styleUnicode), drawLineArt(turtle2, styleAsciiRounded));
