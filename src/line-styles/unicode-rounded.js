b27e1d7dcd0ab236121327c334fafc1c47bae056
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-04-10 18:24:48 -0500

    Fixed a logical bug in the line style.

--- src/line-styles/unicode-rounded.js
+++ src/line-styles/unicode-rounded.js
@@ -4,8 +4,8 @@ export const style = {};
 
 const table11 = {t: '╭┬╮', m: '├┼┤', b: '╰┴╯', v: '│││', h: '───'},
   table12 = {t: '╭╥╮', m: '├╫┤', b: '╰╨╯', v: '│║│', h: '───'},
-  table21 = {t: '╭┬╮', m: '╞╪╡', b: '╰┴╯', v: '│││', h: '─═─'},
-  table22 = {t: '╭╥╮', m: '╞╬╡', b: '╰╨╯', v: '│║│', h: '─═─'};
+  table21 = {t: '╭╤╮', m: '╞╪╡', b: '╰╧╯', v: '│││', h: '─═─'},
+  table22 = {t: '╭╦╮', m: '╞╬╡', b: '╰╩╯', v: '│║│', h: '─═─'};
 
 populateStyle(style, table11, 1, 1);
 populateStyle(style, table12, 1, 2);
