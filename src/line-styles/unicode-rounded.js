ad0d0a0f12a1a86f2dd717d0bcbfe915154a56e5
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-04-11 11:35:03 -0500

    Made the style more predictable.

--- src/line-styles/unicode-rounded.js
+++ src/line-styles/unicode-rounded.js
@@ -3,9 +3,9 @@ import {populateStyle} from './utils.js';
 export const style = {};
 
 const table11 = {t: '╭┬╮', m: '├┼┤', b: '╰┴╯', v: '│││', h: '───'},
-  table12 = {t: '╭╥╮', m: '╟╫╢', b: '╰╨╯', v: '║║║', h: '───'},
-  table21 = {t: '╭╤╮', m: '╞╪╡', b: '╰╧╯', v: '│││', h: '═══'},
-  table22 = {t: '╭╦╮', m: '╠╬╣', b: '╰╩╯', v: '║║║', h: '═══'};
+  table12 = {t: '╓╥╖', m: '╟╫╢', b: '╙╨╜', v: '║║║', h: '───'},
+  table21 = {t: '╒╤╕', m: '╞╪╡', b: '╘╧╛', v: '│││', h: '═══'},
+  table22 = {t: '╔╦╗', m: '╠╬╣', b: '╚╩╝', v: '║║║', h: '═══'};
 
 populateStyle(style, table11, 1, 1);
 populateStyle(style, table12, 1, 2);
