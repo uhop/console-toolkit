b434ded923def18ee3fe83fb89b2c02b928936fe
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-04-11 12:06:14 -0500

    Added table style tests.

---
+++ tests/manual/test-table-style-2.js
@@ -0,0 +1,18 @@
+import {draw as drawTable} from '../../src/table/draw-borders.js';
+import {style as lineStyle} from '../../src/line-styles/unicode-bold.js';
+import {draw} from './utils.js';
+
+const makeBox4 = (hTheme1, hTheme2, vTheme1, vTheme2) =>
+  drawTable(
+    lineStyle,
+    [vTheme1, 1, vTheme1, 1, vTheme1, 1, vTheme2, 1, vTheme2, 1, vTheme2],
+    [hTheme1, 1, hTheme1, 1, hTheme1, 1, hTheme2, 1, hTheme2, 1, hTheme2],
+    {
+      skip: [{x: 3, y: 3, width: 5, height: 5}]
+    }
+  );
+
+const makeBox = (hTheme, vTheme) => makeBox4(hTheme, hTheme, vTheme, vTheme);
+
+draw(makeBox(1, 1), makeBox(1, 2), makeBox(2, 1), makeBox(2, 2));
+draw(makeBox4(1, 2, 1, 2), makeBox4(1, 2, 2, 1), makeBox4(2, 1, 1, 2), makeBox(2, 1, 2, 1));
---
+++ tests/manual/test-table-style-8.js
@@ -0,0 +1,17 @@
+import {draw as drawTable} from '../../src/table/draw-borders.js';
+import {style as lineStyle} from '../../src/line-styles/ascii-dots.js';
+import {draw} from './utils.js';
+
+const makeBox4 = (hTheme1, hTheme2, vTheme1, vTheme2) =>
+  drawTable(
+    lineStyle,
+    [vTheme1, 1, vTheme1, 1, vTheme1, 1, vTheme2, 1, vTheme2, 1, vTheme2],
+    [hTheme1, 1, hTheme1, 1, hTheme1, 1, hTheme2, 1, hTheme2, 1, hTheme2],
+    {
+      skip: [{x: 3, y: 3, width: 5, height: 5}]
+    }
+  );
+
+const makeBox = (hTheme, vTheme) => makeBox4(hTheme, hTheme, vTheme, vTheme);
+
+draw(makeBox(1, 1));
