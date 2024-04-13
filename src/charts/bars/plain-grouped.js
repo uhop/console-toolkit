d8e72079ec735a95b2c3e5a99849920d86568825
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-04-13 17:54:54 -0500

    Added support for gaps and group gaps (for the grouped bars).

--- src/charts/bars/plain-grouped.js
+++ src/charts/bars/plain-grouped.js
@@ -5,26 +5,36 @@ import {drawRow} from './plain.js';
 export const drawChart = (
   values,
   width,
-  {gap = 1, maxValue, theme = defaultTheme, zeroLimit = 0.5, initState = {}} = {}
+  {maxValue, groupGap = 1, gap = 0, rectSize = 0, theme = defaultTheme, zeroLimit = 0.5, initState = {}} = {}
 ) => {
   if (isNaN(width) || width <= 0) throw new Error(`"width" should be positive integer instead of "${width}"`);
 
   const data = normalizeData(values, theme),
     maxSeriesLength = Math.max(0, ...data.map(series => series.length));
 
-  // normalize length of series
-  data.forEach(
-    series => series.length < maxSeriesLength && series.push(...new Array(maxSeriesLength - series.length).fill(null))
-  );
-
+  // normalize length of series and flatten the result
   const newData = [];
-  data.forEach((series, i) => {
-    if (i && gap > 0) newData.push(...new Array(gap).fill(null));
+  data.forEach(series => {
     newData.push(...series);
+    if (series.length < maxSeriesLength) newData.push(...new Array(maxSeriesLength - series.length).fill(null));
   });
 
   const max =
     typeof maxValue == 'number' && maxValue >= 0 ? maxValue : Math.max(0, ...newData.map(datum => datum?.value || 0));
 
-  return newData.map(datum => drawRow([datum], width, max, {zeroLimit, initState}));
+  if (gap < 1 && groupGap < 1)
+    return newData.map(datum => drawRow([datum], width, max, {rectSize, zeroLimit, initState}));
+
+  const result = [];
+  newData.forEach((datum, i) => {
+    if (i) {
+      if (i % maxSeriesLength == 0) {
+        if (groupGap > 0) result.push(new Array(groupGap).fill(''));
+      } else {
+        if (gap > 0) result.push(new Array(gap).fill(''));
+      }
+    }
+    result.push(drawRow([datum], width, max, {rectSize, zeroLimit, initState}));
+  });
+  return result.flat(1);
 };
--- src/charts/bars/plain.js
+++ src/charts/bars/plain.js
@@ -8,7 +8,7 @@ import {makeBgFromFg, normalizeData} from '../utils.js';
 
 const sumValues = row => row.reduce((acc, datum) => acc + (datum?.value || 0), 0);
 
-export const drawRow = (data, width, maxValue, {zeroLimit = 0.5, initState = {}}) => {
+export const drawRow = (data, width, maxValue, {rectSize = 0, zeroLimit = 0.5, initState = {}}) => {
   const normalize = maxValue < 0;
   if (maxValue < 0) maxValue = sumValues(data);
   if (!maxValue) maxValue = 1;
@@ -33,12 +33,23 @@ export const drawRow = (data, width, maxValue, {zeroLimit = 0.5, initState = {}}
   );
 };
 
-export const drawChart = (values, width, {maxValue, theme = defaultTheme, zeroLimit = 0.5, initState = {}} = {}) => {
+export const drawChart = (
+  values,
+  width,
+  {maxValue, gap = 0, rectSize = 0, theme = defaultTheme, zeroLimit = 0.5, initState = {}} = {}
+) => {
   if (isNaN(width) || width <= 0) throw new Error(`"width" should be positive integer instead of "${width}"`);
 
   const data = normalizeData(values, theme),
     max =
       typeof maxValue == 'number' ? (maxValue < 0 ? -1 : maxValue) : Math.max(0, ...data.map(row => sumValues(row)));
 
-  return data.map(row => drawRow(row, width, max, {zeroLimit, initState}));
+  if (gap < 1) return data.map(row => drawRow(row, width, max, {rectSize, zeroLimit, initState})).flat(1);
+
+  const result = [];
+  data.forEach((row, i) => {
+    if (i) result.push(...new Array(gap).fill(''));
+    result.push(drawRow(row, width, max, {rectSize, zeroLimit, initState}));
+  });
+  return result.flat(1);
 };
