0885fe54bbf9b73d7fa374809a4fd74dc2d96dfe
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-04-12 23:45:38 -0500

    Renamed.

--- tests/manual/test-block-0.js
+++ tests/manual/test-block-0.js
@@ -1,5 +1,5 @@
 import {draw as drawTable} from '../../src/table/draw-borders.js';
-import lineTheme from '../../src/line-themes/unicode-block.js';
+import lineTheme from '../../src/line-themes/unicode-half-block.js';
 import drawFrame from '../../src/draw-frame.js';
 import {draw} from './utils.js';
 
