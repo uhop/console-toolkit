b82834d1ae320d84657ae322c901c17334556717
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-04-16 19:41:11 -0500

    Support for right-aligned block-based charts + a test.

--- src/charts/bars/block.js
+++ src/charts/bars/block.js
@@ -13,14 +13,17 @@ export const drawRow = (
   data,
   width,
   maxValue,
-  {t = 1, r = 1, b = 1, l = 0, blockTheme = defaultBlockTheme, rectSize = 0, initState = {}}
+  options = {}
 ) => {
-  const sizes = allocateSizes(data, maxValue, width),
-    blocks = data.map((datum, i) => {
+  const {reverse, blockTheme = defaultBlockTheme, rectSize = 0, initState = {}} = options,
+    sizes = allocateSizes(data, maxValue, width),
+    {t = 1, b = 1, l = (reverse ? 1 : 0), r = (reverse ? 0 : 1)} = options;
+
+  const blocks = data.map((datum, i) => {
       if (!datum) return Box.makeBlank(0, Math.max(2, rectSize));
       const box = drawBlock(sizes[i], Math.max(0, rectSize - 2), blockTheme, {
-          left: i ? 0 : l,
-          right: i + 1 < data.length ? 0 : r,
+          left: reverse ? (i + 1 < data.length ? 0: l) : (i ? 0 : l),
+          right: reverse ? (i ? 0 : r) : (i + 1 < data.length ? 0 : r),
           top: t,
           bottom: b
         }),
@@ -29,8 +32,9 @@ export const drawRow = (
         box.box.map(line => boxStyle.text(line)),
         true
       );
-    });
-  const result = new Array(blocks[0].box.length).fill('');
+    }),
+    result = new Array(blocks[0].box.length).fill('');
+  if (reverse) blocks.reverse();
   for (const block of blocks) {
     for (let i = 0; i < block.box.length; ++i) {
       result[i] += block.box[i];
---
+++ tests/manual/test-chart-bars-plain-5.js
@@ -0,0 +1,33 @@
+import Box from '../../src/Box.js';
+import drawStackedChart from '../../src/charts/bars/block.js';
+import drawGroupedChart from '../../src/charts/bars/block-grouped.js';
+import {draw} from './utils.js';
+
+console.log('Butterfly stacked bars:');
+draw(
+  Box.make(
+    drawStackedChart(
+      [
+        [2, 1, 2],
+        [5, 1, 4],
+        [1, 1],
+        [3, 1, 3]
+      ],
+      20,
+      {reverse: true}
+    ),
+    {align: 'right'}
+  ),
+  drawStackedChart(
+    [
+      [2, 1, 2],
+      [5, 1, 4],
+      [1, 1],
+      [3, 1, 3]
+    ],
+    20
+  )
+);
+
+console.log('Butterfly grouped bars:');
+draw(Box.make(drawGroupedChart([[2, 4, 3]], 20, {reverse: true}), {align: 'right'}), drawGroupedChart([[2, 4, 3]], 20));
