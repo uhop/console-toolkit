3cf8e63f0457d6ab4e5ba3f51fca8fee61ee3cd7
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-04-22 17:14:24 -0500

    Added tests for `drawBlock()` and related procedures.

---
+++ tests/test-draw-block.js
@@ -0,0 +1,52 @@
+import test from 'tape-six';
+
+import {drawFrame, drawBlock} from '../src/draw-block.js';
+import lineTheme from '../src/themes/lines/ascii-girder.js';
+import blockTheme from '../src/themes/blocks/unicode-half.js';
+
+test('Draw a block', async t => {
+  await t.test('Block', t => {
+    const b = drawBlock(2, 1, blockTheme);
+
+    t.equal(b.width, 4);
+    t.equal(b.height, 3);
+
+    t.deepEqual(b.box, ['▗▄▄▖', '▐██▌', '▝▀▀▘']);
+  });
+
+  await t.test('Frame', t => {
+    const b = drawFrame(2, 1, blockTheme);
+
+    t.equal(b.width, 4);
+    t.equal(b.height, 3);
+
+    t.deepEqual(b.box, ['▗▄▄▖', '▐  ▌', '▝▀▀▘']);
+  });
+
+  await t.test('Block with lines', t => {
+    const b = drawBlock(2, 1, lineTheme);
+
+    t.equal(b.width, 6);
+    t.equal(b.height, 3);
+
+    t.deepEqual(b.box, ['//==\\\\', '||  ||', '\\\\==//']);
+  });
+
+  await t.test('Block with lines and a filler', t => {
+    const b = drawBlock(2, 1, lineTheme, {symbol: '*'});
+
+    t.equal(b.width, 6);
+    t.equal(b.height, 3);
+
+    t.deepEqual(b.box, ['//==\\\\', '||**||', '\\\\==//']);
+  });
+
+  await t.test('Frame with zero lines', t => {
+    const b = drawBlock(2, 1, lineTheme, {left: 0, right: 0});
+
+    t.equal(b.width,2);
+    t.equal(b.height, 3);
+
+    t.deepEqual(b.box, ['==', '  ', '==']);
+  });
+});
