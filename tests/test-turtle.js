97ce27f83f385d4322631a510fde2c763ddf86fe
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-04-23 17:08:53 -0500

    More Turtle tests.

--- tests/test-turtle.js
+++ tests/test-turtle.js
@@ -1,15 +1,40 @@
 import test from 'tape-six';
 
 import lineTheme from '../src/themes/lines/unicode.js';
+import lineAsciiTheme from '../src/themes/lines/ascii-rounded.js';
 import {draw as drawBorders} from '../src/table/index.js';
-import {Turtle, draw as drawTurtle} from '../src/turtle/index.js';
+import Turtle, {draw as drawTurtle} from '../src/turtle/index.js';
 import drawLineArt from '../src/turtle/draw-line-art.js';
+import Panel from '../src/Panel.js';
 
 test('Turtle', async t => {
   await t.test('Draw an axis', t => {
     const turtle = new Turtle(5, 1).markHalfDown().forward(2).markHalfUp().forward(2).markHalfDown(),
       axis = drawLineArt(turtle, lineTheme);
     t.deepEqual(axis.box, ['┌─┴─┐']);
+  });
+
+  await t.test('Draw a box with a cross (Unicode)', t => {
+    const turtle = new Turtle(5, 5)
+      .forward(4)
+      .right()
+      .forward(4)
+      .right()
+      .forward(4)
+      .right()
+      .forward(4)
+      .set(2, 0)
+      .moveDown(4)
+      .setTheme(2)
+      .set(0, 0)
+      .moveRight(4)
+      .set(0, 2)
+      .moveRight(4)
+      .set(0, 4)
+      .moveRight(4);
+
+    t.deepEqual(drawLineArt(turtle, lineTheme).box, ['╒═╤═╕', '│ │ │', '╞═╪═╡', '│ │ │', '╘═╧═╛']);
+    t.deepEqual(drawLineArt(turtle, lineAsciiTheme).box, ['.=.=.', '| | |', ':=+=:', '| | |', "'='='"]);
   });
 
   await t.test('Compare with table', t => {
@@ -66,5 +91,50 @@ test('Turtle', async t => {
     );
 
     t.deepEqual(turtle.box, table.box);
+  });
+
+  await t.test('A tree', t => {
+    const turtle = new Turtle(5, 1).markHalfDown().forward(2).markHalfUp().forward(2).markHalfDown(),
+      branch = drawLineArt(turtle, lineTheme),
+      panel = new Panel(7, 5)
+        .put(2, 1, branch)
+        .put(4, 0, '2')
+        .put(2, 2, '4')
+        .put(6, 2, '5')
+        .put(0, 3, branch)
+        .put(0, 4, '1')
+        .put(4, 4, '3');
+
+    t.deepEqual(panel.toBox().box, [
+      '\x1B[m    2  ',
+      '\x1B[m  ┌─┴─┐',
+      '\x1B[m  4   5',
+      '┌─┴─┐\x1B[m  ',
+      '1\x1B[m   3  '
+    ]);
+  });
+
+  await t.test('A fancy table', t => {
+    const turtle = new Turtle(5, 5)
+      .forward(4)
+      .right()
+      .forward(4)
+      .right()
+      .forward(4)
+      .right()
+      .forward(4)
+      .setTheme(2)
+      .set(2, 0)
+      .moveDown(4)
+      .set(0, 2)
+      .moveRight(4)
+      .setTheme(1)
+      .set(2, 0)
+      .moveDown(2)
+      .set(0, 2)
+      .moveRight(2);
+
+    t.deepEqual(drawTurtle(turtle).box, ['┌─┬─┐', '│ │ │', '├─╆━┥', '│ ┃ │', '└─┸─┘']);
+    t.deepEqual(drawTurtle(turtle, {useArcs: true}).box, ['╭─┬─╮', '│ │ │', '├─╆━┥', '│ ┃ │', '╰─┸─╯']);
   });
 });
