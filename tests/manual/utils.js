febedf28c14a69f7e12715c7b7f28de9722604df
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-04-30 13:15:27 -0500

    Simplified `draw()`.

--- tests/manual/utils.js
+++ tests/manual/utils.js
@@ -1,24 +1,19 @@
 import Box from '../../src/box.js';
 
-export const drawBox = box => box.box.forEach(line => console.log(line));
+export const drawStrings = strings => strings.forEach(line => console.log(line));
 
 export const draw = (...boxes) => {
   if (!boxes.length) return;
 
   let result;
   for (const box of boxes) {
-    let b;
-    if (box instanceof Box) {
-      b = box;
-    } else if (typeof box?.toBox == 'function') {
-      b = box.toBox();
-    } else {
-      b = new Box(box);
-    }
+    const b = Box.make(box);
     result = result ? result.addRight(b.padLeft(2)) : b;
   }
-  drawBox(result);
+  result && drawStrings(result.toStrings());
 };
 
-export const show = s =>
-  console.log(s.replace(/[\x00-\x1F]/g, m => '\\x' + m[0].charCodeAt(0).toString(16).padStart(2, '0').toUpperCase()));
+export const show = string =>
+  console.log(
+    string.replace(/[\x00-\x1F]/g, m => '\\x' + m[0].charCodeAt(0).toString(16).padStart(2, '0').toUpperCase())
+  );
