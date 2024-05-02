7ee60b5f2a1d1630cc2e8631535c41f9c05e9c74
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-05-01 20:31:58 -0500

    Added more symbols and a formatter:
    * Added superscripts and subscripts.
    * Added more overlays for circled numbers up to 50.
    * Added a formatter for exponential numbers.
      * It uses new symbols and superscript numbers.
    * Added tests for the formatter.

--- src/alphanumeric/unicode-numbers.js
+++ src/alphanumeric/unicode-numbers.js
@@ -1,4 +1,5 @@
 import {SymbolRange, transcode as internalTranscode} from './utils.js';
+import {minus, multiplication, superscriptPlus, superscriptMinus} from '../symbols.js';
 
 export const transcodeTables = {
   // Enclosed Alphanumeric
@@ -24,7 +25,11 @@ export const transcodeTables = {
   // Dingbats
   dingbatsCircledSansSerif: new SymbolRange('➀', 1, 10),
   dingbatsNegativeCircled: new SymbolRange('❶', 1, 10),
-  dingbatsNegativeCircledSansSerif: new SymbolRange('➊', 1, 10)
+  dingbatsNegativeCircledSansSerif: new SymbolRange('➊', 1, 10),
+
+  // Superscripts and Subscripts
+  superscript: new SymbolRange('⁰'),
+  subscript: new SymbolRange('₀')
 };
 
 // const numbersWithDots = ['🄀'];
@@ -32,12 +37,25 @@ export const transcodeTables = {
 
 // patches
 
-transcodeTables.circled.overlay = {0: '\u{24EA}'};
+const circled_21_35 = new SymbolRange('\u{3251}', 21, 35),
+  circled_36_50 = new SymbolRange('\u{32B1}', 36, 50);
+circled_36_50.overlay = {0: '\u{24EA}'};
+circled_21_35.overlay = circled_36_50;
+transcodeTables.circled.overlay = circled_21_35;
+
 const negativeCircled_11_20 = new SymbolRange('\u{24EB}', 11, 20);
 negativeCircled_11_20.overlay = {0: '\u{24FF}'};
 transcodeTables.dingbatsNegativeCircled.overlay = negativeCircled_11_20;
 
+transcodeTables.superscript.overlay = {1: '¹', 2: '²', 3: '³'};
+
 // API
+
+export const transcode = (s, name, options) => {
+  const table = typeof name == 'string' ? transcodeTables[name] : name;
+  if (!table) throw new Error(`There is no transcode table "${name}"`);
+  return internalTranscode(s, table, options);
+};
 
 export const numberPunctuation = (s, {addBefore = '', addAfter = ' '} = {}) =>
   s.replace(
@@ -45,8 +63,14 @@ export const numberPunctuation = (s, {addBefore = '', addAfter = ' '} = {}) =>
     (_, d, p) => addBefore + transcodeTables[p === '.' ? 'dots' : 'commas'].transcode(d) + addAfter
   );
 
-export const transcode = (s, name, options) => {
-  const table = typeof name == 'string' ? transcodeTables[name] : name;
-  if (!table) throw new Error(`There is no transcode table "${name}"`);
-  return internalTranscode(s, table, options);
+export const numberExponent = (s, {useSpecialMinus} = {}) => {
+  const r = /^([+-]?)([^e]+)e([+-]?)(.+)$/i.exec(s);
+  return r
+    ? (r[1] === '-' && useSpecialMinus ? minus : r[1]) +
+        r[2] +
+        multiplication +
+        '10' +
+        (r[3] === '+' ? superscriptPlus : r[3] === '-' ? superscriptMinus : r[3]) +
+        transcode(r[4], transcodeTables.superscript)
+    : s;
 };
--- tests/manual/test-alphanum-0.js
+++ tests/manual/test-alphanum-0.js
@@ -1,8 +1,28 @@
 import {formatNumber} from '../../src/alphanumeric/number-formatters.js';
-import {numberPunctuation} from '../../src/alphanumeric/unicode-numbers.js';
+import {numberPunctuation, numberExponent} from '../../src/alphanumeric/unicode-numbers.js';
 
-const sample = 123456789.123456;
+const sample1 = 123456789.123456;
 
-console.log(sample);
-console.log(formatNumber(sample, {decimals: 6}));
-console.log(numberPunctuation(formatNumber(sample, {decimals: 6})));
+console.log(sample1);
+console.log(formatNumber(sample1, {decimals: 6}));
+console.log(numberPunctuation(formatNumber(sample1, {decimals: 6})));
+
+const sample2 = 1.23456789e33;
+console.log();
+console.log(sample2);
+console.log(numberExponent(sample2));
+
+const sample3 = -1.23456789e33;
+console.log();
+console.log(sample3);
+console.log(numberExponent(sample3));
+
+const sample4 = -1.23456789e33;
+console.log();
+console.log(sample4);
+console.log(numberExponent(sample4, {useSpecialMinus: true}));
+
+const sample5 = 1.23456789e-33;
+console.log();
+console.log(sample5);
+console.log(numberExponent(sample5));
