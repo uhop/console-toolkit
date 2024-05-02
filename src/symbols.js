3822dab1bdc10c3f1e8065140e3295ec8822ea82
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-05-01 20:27:31 -0500

    Added math symbols.

--- src/symbols.js
+++ src/symbols.js
@@ -64,6 +64,8 @@ export const quadrants = [
 
 export const quadrant = (tl, tr, bl, br) => quadrants[(tl ? 1 : 0) + (tr ? 2 : 0) + (bl ? 4 : 0) + (br ? 8 : 0)];
 
+// shades
+
 export const shades = [' ', ...generateSequence(0x2591, 0, 2), fullBlock];
 
 export const shadeLight = shades[1];
@@ -71,6 +73,7 @@ export const shadeMedium = shades[2];
 export const shadeDark = shades[3];
 
 // ellipses
+
 export const hellip = '\u{2026}'; // horizontal ellipsis
 export const vellip = '\u{22EE}'; // vertical ellipsis
 export const ctdot = '\u{22EF}'; // midline horizontal ellipsis
@@ -78,3 +81,23 @@ export const utdot = '\u{22F0}'; // up right diagonal ellipsis
 export const dtdot = '\u{22F1}'; // down right diagonal ellipsis
 
 export {hellip as ellipsis};
+
+// math
+
+export const infinity = '\u{221e}';
+export const plusMinus = '\u{00B1}';
+export const minusPlus = '\u{2213}';
+export const tilde = '\u{223C}'
+export const minus = '\u{2212}';
+export const multiplication = '\u{00D7}';
+export const division = '\u{00F7}';
+export const product = '\u{220F}';
+export const sum = '\u{2211}';
+export const forAll = '\u{2200}';
+export const exist = '\u{2203}';
+export const degree = '\u{00B0}';
+
+export const superscriptPlus = '\u{207A}';
+export const superscriptMinus = '\u{207B}';
+export const subscriptPlus = '\u{208A}';
+export const subscriptMinus = '\u{208B}';
