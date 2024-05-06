30ef4bde72772f39ab633b2b27e55012a5ca7736
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-05-05 21:41:47 -0500

    A face-lift for show functions.

--- src/show.js
+++ src/show.js
@@ -2,17 +2,10 @@ import process from 'node:process';
 import {matchCsiNoGroups} from './strings.js';
 import Box from './box.js';
 
-const toBox = s => {
-  if (s instanceof Box) return s;
-  if (s && typeof s == 'object' && typeof s.toBox == 'function') {
-    return s.toBox({emptySymbol: ' '});
-  }
-  return new Box(s);
-};
-
 export const log = (s, {endOfLineCommand = '\x1B[m', colorDepth = 24} = {}) => {
-  s = toBox(s);
+  s = Box.make(s);
   if (colorDepth < 4) {
+    matchCsiNoGroups.lastIndex = 0;
     s.box.forEach(row => console.log((row + endOfLineCommand).replace(matchCsiNoGroups, '')));
     return;
   }
@@ -20,9 +13,10 @@ export const log = (s, {endOfLineCommand = '\x1B[m', colorDepth = 24} = {}) => {
 };
 
 export const out = (s, {stream = process.stdout, endOfLineCommand = '\x1B[m', colorDepth} = {}) => {
-  s = toBox(s);
-  if (typeof colorDepth != 'number') colorDepth = stream.isTTY ? stream.getColorDepth() : 1;
+  s = Box.make(s);
+  if (typeof colorDepth != 'number' || isNaN(colorDepth)) colorDepth = stream.isTTY ? stream.getColorDepth() : 1;
   if (colorDepth < 4) {
+    matchCsiNoGroups.lastIndex = 0;
     s.box.forEach(row => stream.write((row + endOfLineCommand).replace(matchCsiNoGroups, '') + '\n'));
     return;
   }
@@ -35,7 +29,7 @@ export class Out {
     this.colorDepth = stream.isTTY ? stream.getColorDepth() : 1;
   }
   out(s, {endOfLineCommand = '\x1B[m', colorDepth} = {}) {
-    if (typeof colorDepth != 'number') colorDepth = this.colorDepth;
+    if (typeof colorDepth != 'number' || isNaN(colorDepth)) colorDepth = this.colorDepth;
     return out(s, this.stream, endOfLineCommand, colorDepth);
   }
 }
