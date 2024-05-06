9e396686a703fdffe721a2fc1111b75d7502bc89
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-05-05 21:18:40 -0500

    Added styles.

--- tests/manual/test-writer-0.js
+++ tests/manual/test-writer-0.js
@@ -1,27 +1,32 @@
-import {CLEAR_SCREEN, CURSOR_RESTORE_POS, CURSOR_SAVE_POS, SCREEN_RESTORE, SCREEN_SAVE} from '../../src/ansi/csi.js';
+import {
+  CLEAR_SCREEN,
+  CURSOR_HOME,
+  CURSOR_INVISIBLE,
+  CURSOR_NORMAL,
+  CURSOR_RESTORE_POS,
+  CURSOR_SAVE_POS,
+  SCREEN_RESTORE,
+  SCREEN_SAVE
+} from '../../src/ansi/index.js';
+import style from '../../src/style.js';
 import Writer from '../../src/writer.js';
 
 const sleep = (ms = 1000) => new Promise(resolve => setTimeout(resolve, ms));
 
 const writer = new Writer(process.stdout);
 
-await writer.writeString(SCREEN_SAVE + CURSOR_SAVE_POS + CLEAR_SCREEN);
+await writer.writeString(SCREEN_SAVE + CURSOR_SAVE_POS + CLEAR_SCREEN + CURSOR_INVISIBLE + CURSOR_HOME);
+// await writer.cursorTo(0, 0);
 
-await sleep();
-
-await writer.cursorTo(0, 0);
-
-await sleep();
-
-await writer.writeString('123');
-await writer.write(['xxx', 'yy', 'z']);
+await writer.writeString(style.inverse.text('123'));
+await writer.write(style.bg.blue.bold.bright.white.text(['xxx', 'yy', 'z']));
 
 await writer.cursorTo(10, 10);
-await writer.write(['xxx', 'yy', 'z'], true);
+await writer.write(style.bold.bright.green.text(['xxx', 'yy', 'z']), true);
 
 await writer.moveCursor(10, -3);
-await writer.write(['xxx', 'yy', 'z'], true);
+await writer.write(style.bold.bright.red.text('xxx\nyy\nz'), true);
 
-await sleep();
+await sleep(2000);
 
-await writer.writeString(CURSOR_RESTORE_POS + SCREEN_RESTORE);
+await writer.writeString(CURSOR_RESTORE_POS + SCREEN_RESTORE + CURSOR_NORMAL);
