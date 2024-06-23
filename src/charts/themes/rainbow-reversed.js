0ac9d9b42feb8e442db37fb057a6ce5edd079dcd
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-06-23 00:32:39 -0500

    Added two rainbow-based chart themes.

---
+++ src/charts/themes/rainbow-reversed.js
@@ -0,0 +1,5 @@
+import rainbow from "./rainbow.js";
+
+export const chartTheme = rainbow.slice().reverse();
+
+export default chartTheme;
---
+++ src/charts/themes/rainbow.js
@@ -0,0 +1,8 @@
+import style from '../../style.js';
+
+// red, orange, yellow, green, blue, indigo, violet
+const colors = [0xff0000, 0xffa500, 0xffff00, 0x008000, 0x0000ff, 0x4b0082, 0xee82ee];
+
+export const chartTheme = colors.map(color => ({colorState: style.hex(color).getState()}));
+
+export default chartTheme;
