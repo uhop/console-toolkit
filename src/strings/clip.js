5e8938dc6d8a4fc71fe639bdfdd5fe7a5b9810d1
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-07-01 14:37:07 -0500

    Refactored `clip()` fixing bugs.

--- src/strings/clip.js
+++ src/strings/clip.js
@@ -2,23 +2,27 @@ import parse, {matchCsiNoGroups} from './parse.js';
 import {split} from './split.js';
 
 export const clip = (s, width, options = {}) => {
-  if (width <= 0) return '';
-
   const {includeLastCommand = false, matcher = matchCsiNoGroups} = options;
 
   let counter = 0;
   for (const {start, string, match} of parse(s, matcher)) {
-    if (counter >= width) return match ? s.substring(0, match.index + (includeLastCommand ? match[0].length : 0)) : s;
-    const prev = split(string, options);
-    counter += prev.width;
-    if (includeLastCommand ? counter > width : counter >= width) {
-      let result = '';
-      for (const grapheme of prev.graphemes) {
-        if (result.length + grapheme.width > width) break;
-        result += grapheme.symbol;
-      }
-      return s.substring(0, start) + result;
+    if (counter >= width)
+      return match ? s.substring(0, match.index + (includeLastCommand ? match[0].length : 0)) : s.substring(0, start);
+    const prev = split(string, options),
+      newCounter = counter + prev.width;
+    if (newCounter === width)
+      return match ? s.substring(0, match.index + (includeLastCommand ? match[0].length : 0)) : s;
+    if (newCounter < width) {
+      counter = newCounter;
+      continue;
     }
+    let result = '';
+    for (const grapheme of prev.graphemes) {
+      if (counter + grapheme.width > width) break;
+      result += grapheme.symbol;
+      counter += grapheme.width;
+    }
+    return s.substring(0, start) + result;
   }
 
   return s;
